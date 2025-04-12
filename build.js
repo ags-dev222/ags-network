3169a5e13983c94adeaf3b01ae9ad5e81e8a0772
Nana-Boaheng1 <salimbineddy16@gmail.com>
2025-04-12 13:35:26 +0000

    build

---
+++ build.js
@@ -0,0 +1,11 @@
+// filepath: s:\CS\AGS-Eco\build.js
+import { build } from "vite";
+
+build()
+  .then(() => {
+    console.log("Build completed successfully.");
+  })
+  .catch((err) => {
+    console.error("Build failed:", err);
+    process.exit(1);
+  });
